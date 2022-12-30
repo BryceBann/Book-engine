f31e53f6a90aae43e2440b6a40cd8ef56c025eef
BryceBann <109399715+BryceBann@users.noreply.github.com>
2022-12-30 14:38:18 -0600

    not sure why log in not working

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const path = require('path');
-const db = require('./config/connection');
+
 // const routes = require('./routes');
 const { ApolloServer } = require('apollo-server-express');
 
 const { typeDefs, resolvers } = require('./schema')
+const db = require('./config/connection');
 
 const {authMiddleware} = require('./utils/auth');
 
@@ -19,7 +20,7 @@ const server = new ApolloServer({
 
 server.applyMiddleware({ app });
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
